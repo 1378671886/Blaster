6667626c0dfc336021b6619c258948b5eb5ed168
1378671886 <1378671886@qq.com>
2026-05-02 19:40:03 +0800

    保存一个框架包

--- Source/Blaster/Private/Component/LagCompensationComponent.cpp
+++ Source/Blaster/Private/Component/LagCompensationComponent.cpp
@@ -1,7 +1,9 @@
 
 
 #include "Component/LagCompensationComponent.h"
-
+#include "Character/BlasterCharacter.h"
+#include "Components/BoxComponent.h"
+#include "DrawDebugHelpers.h"
 
 ULagCompensationComponent::ULagCompensationComponent()
 {
@@ -13,12 +15,49 @@ void ULagCompensationComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
+	FFramePackage FramePackage;
+	SaveFramePackage(FramePackage);
+	ShowFramePackage(FramePackage, FColor::Red);
 	
+}
+
+void ULagCompensationComponent::SaveFramePackage(FFramePackage& Package)
+{
+	Character = Character == nullptr ? Cast<ABlasterCharacter>(GetOwner()) : Character;
+	if (Character)
+	{
+		Package.Time = GetWorld()->GetTimeSeconds();
+		Package.Character = Character;
+		for (auto& BoxPair : Character->HitCollisionBoxes)
+		{
+			FBoxInformation BoxInformation;
+			BoxInformation.Location = BoxPair.Value->GetComponentLocation();
+			BoxInformation.Rotation = BoxPair.Value->GetComponentRotation();
+			BoxInformation.BoxExtent = BoxPair.Value->GetScaledBoxExtent();
+			Package.HitBoxInfo.Add(BoxPair.Key, BoxInformation);
+		}
+	}
 }
 
 void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+}
+
+void ULagCompensationComponent::ShowFramePackage(const FFramePackage& Package, FColor Color)
+{
+	for (auto& BoxInfo : Package.HitBoxInfo)
+	{
+		DrawDebugBox(
+			GetWorld(),
+			BoxInfo.Value.Location,
+			BoxInfo.Value.BoxExtent,
+			FQuat(BoxInfo.Value.Rotation),
+			Color,
+			false,
+			4.f
+		);
+	}
 }
 
